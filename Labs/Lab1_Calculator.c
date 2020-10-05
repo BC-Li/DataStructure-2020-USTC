fefffc3e96fff044acf3b24c7459d031e0b4a170
BC-Li <251156266@qq.com>
2020-10-05 16:25:02 +0800

    Update Lab 1

--- Labs/Lab1_Calculator.c
+++ Labs/Lab1_Calculator.c
@@ -57,6 +57,8 @@ int ListInsert(float e, int n, SqList *L ){
     
 }
 
+// Calculate functions for LinkedList 
+
 int ListAddition(SqList *A, SqList *B, SqList *Addition){
     SqList *searcher_A = A,*searcher_B = B;
     int n = 0;
@@ -105,18 +107,37 @@ int ListSubtraction(SqList *A,SqList *B, SqList *Substraction){    // A-B
     
     return OK;
 }
+// WIP features
+
+// int ListMultiply(SqList *A, SqList *B){
+// } 
+
+//  Standard Output and Other Features
 
 int PrintList(SqList *A){
-    SqList *Traveler = A;
+    SqList *Traveler = A->next;
     if(Traveler->next == NULL){
         return ERROR;   // the list is empty or something else
     }
-    while(Traveler != NULL){
-        if(Traveler->data>0){
-        printf("+%fx^%d",Traveler->data,Traveler->index);
-        } 
+
+    // Head of the polynomial, add some special judgements 
+    if(Traveler->data > 0){     
         printf("%fx^%d",Traveler->data,Traveler->index);
     }
+    if(Traveler->data > 0){     
+        printf("-%fx^%d",Traveler->data,Traveler->index);
+    }
+    
+    // Print the remaining factors
+    while(Traveler != NULL){
+        if(Traveler->data > 0){
+            printf("+%fx^%d",Traveler->data,Traveler->index);
+        } 
+        if(Traveler->data < 0){
+            printf("%fx^%d",Traveler->data,Traveler->index);
+        }
+    }
+
     return OK;
 }
 
@@ -143,7 +164,68 @@ int CalculateValue(SqList *A, float x){     // Calculate the value of f(x) (give
     return OK;
 }
 
+void welcome()
+{
+	printf ("\n");
+	printf ("\n");
+	printf ("\n");
+    printf("***************************Unary sparse polynomial calculator By Bocheng Li*****************************");
+    printf("***************************View https://github.com/BC-Li for more info.*********************************");
+    printf("\n");
+    printf("\n");
+    printf("********************************    1.Addition      ***************************************\n");
+    printf("********************************    2.Subtraction      ***************************************\n");
+    printf("********************************    3.Multiply      ***************************************\n");
+    printf("********************************    4.Display  ***************************************\n");
+    printf("********************************    0.EXIT    ***************************************\n\n\n");
+ 
+}
+int main() 
+{ 
+    
 
+    char  choice[100];
+    int choice_1;
+    char  yes_no;
+    
+    
+    do
+    {
+        welcome();
+    printf("*******************************    Input your command please~ 0-5：  **************************************\n");
+        scanf("%s",choice);
+        choice_1=choice[0]-48;
+        printf("\n");
+        switch(choice_1)
+       {
+            case 1:head_1=add(head_1,tail_1,pnew_1);
+			       
+            break;
+            case 2:search_name(head_1);
+            break;
+            case 3:listdelete(head_1);
+            break;
+            case 4:Display_Linkedlist(head_1);
+            break;
+            case 0:
+    printf("********************************         Bye~      ***************************************\n");
+            exit_all(&head_1);
+            getchar();
+            exit(0);  //正常退出
+            break;
+            default:
+    printf("****************************输入错误......请重新输入哦***********************************\n");
+             break;
+        }
+    printf("****************************  是否继续  Y  or   N?   ************************************\n");
+         do
+        {
+           scanf("%c",&yes_no);
+        }
+	while(yes_no!='Y'&&yes_no!='y'&&yes_no!='N'&&yes_no!='n');
+    }
+    while(yes_no=='Y'||yes_no=='y');
+}
 
 
 
