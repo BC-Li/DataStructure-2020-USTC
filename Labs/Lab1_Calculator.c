77e21c3a42846fe7ade5e69267de78fe44eb293d
BC-Li <251156266@qq.com>
2020-10-06 08:45:13 +0800

    Update Lab1

--- Labs/Lab1_Calculator.c
+++ Labs/Lab1_Calculator.c
@@ -27,6 +27,7 @@ int ListInit(SqList *L){
         return ERROR;
     }
     L->next = NULL;
+    printf("Please input the coefficient and index. following order like '1 2'. press ctrl+z to stop.");
     while((scanf("%d",&coefficient)!= EOF)&&(scanf("%d",&index_filler)!= EOF)){
         p = (SqList *)malloc(sizeof(SqList));
         p->data = coefficient;
@@ -61,12 +62,12 @@ int ListInsert(float e, int n, SqList *L ){
     p->index = n;
 
     end = L;
-    while(end!=NULL){
+    while(end->next!=NULL){
         end = end->next;
     }
     end->next = p;
     return OK;
-    
+    printf("Insert ready.\n");
 }
 
 // Calculate functions for LinkedList 
@@ -119,11 +120,10 @@ int ListSubtraction(SqList *A,SqList *B, SqList *Substraction){    // A-B
     
     return OK;
 }
-// WIP features
 
 int ListMultiply(SqList *A, SqList *B, SqList *MultiplyTemp, SqList *Multiply_B, SqList *Output){
     OutputListInit(MultiplyTemp);
-    
+    OutputListInit(Output);
     SqList *searcher_A = A, *searcher_B = B;
     while(searcher_A!=NULL){
         while(searcher_B!=NULL){
@@ -189,19 +189,15 @@ int CalculateValue(SqList *A, float x){     // Calculate the value of f(x) (give
     return OK;
 }
 
-int ListSort(SqList *A){
-    SqList *p,*q;
-    p = A;
-    q = p->next;
-    while(p!=NULL){
-        while(q!=NULL){
-            if(q->index>p->index){
-                 
-            }
-        }
+int ListFree(SqList *L){
+    SqList *p;
+    while(L!=NULL){
+        p = L->next;
+        free(L);
+        L = p;
     }
+    printf("free.\n");
 }
-
 void welcome(){
 	printf ("\n");
 	printf ("\n");
@@ -219,76 +215,78 @@ void welcome(){
 }
 int main() 
 { 
+    SqList A,B,Addition;
+    ListAddition(&A,&B,&Addition);
+    PrintList(&Addition);
+    return 0;
+    // char  choice[100];
+    // int choice_1;
+    // char  yes_no;
     
-
-    char  choice[100];
-    int choice_1;
-    char  yes_no;
     
-    
-    do
-    {
-        welcome();
-    printf("*******************************    Input your command  0-5:  **************************************\n");
-        scanf("%s",choice);
-        choice_1=choice[0]-48;
-        printf("\n");
-        switch(choice_1)
-       {
-            case 1:{
-                SqList A, B;
-                ListInit(&A);
-                ListInit(&B);
-                break;
-            }
-            case 2:{
-                SqList A, B;
-                ListInit(&A);
-                ListInit(&B);
-                SqList AdditionResult;
-                ListAddition(&A,&B,&AdditionResult);
-                PrintList(&AdditionResult);
-                break;
-            }
-            case 3:{
-                SqList A, B;
-                ListInit(&A);
-                ListInit(&B);
-                SqList SubtractionResult;
-                ListSubtraction(&A,&B,&SubtractionResult);
-                PrintList(&SubtractionResult);
-                break;
-            }
-            case 4:{
-                SqList A, B;
-                ListInit(&A);
-                ListInit(&B);
-                SqList MultiplyResult;
-                ListMultiply(&A,&B,&MultiplyResult);
-                PrintList(&MultiplyResult);
-                break;
-            }
+    // do
+    // {
+    //     welcome();
+    // printf("*******************************    Input your command  0-5:  **************************************\n");
+    //     scanf("%s",choice);
+    //     choice_1=choice[0]-48;
+    //     printf("\n");
+    //     switch(choice_1)
+    //    {
+    //         case 1:{
+    //             SqList A, B;
+    //             ListInit(&A);
+    //             ListInit(&B);
+    //             break;
+    //         }
+    //         case 2:{
+    //             SqList A, B;
+    //             ListInit(&A);
+    //             ListInit(&B);
+    //             SqList AdditionResult;
+    //             ListAddition(&A,&B,&AdditionResult);
+    //             PrintList(&AdditionResult);
+    //             break;
+    //         }
+    //         case 3:{
+    //             SqList A, B;
+    //             ListInit(&A);
+    //             ListInit(&B);
+    //             SqList SubtractionResult;
+    //             ListSubtraction(&A,&B,&SubtractionResult);
+    //             PrintList(&SubtractionResult);
+    //             break;
+    //         }
+    //         case 4:{
+    //             SqList A, B;
+    //             ListInit(&A);
+    //             ListInit(&B);
+    //             SqList MultiplyResult;
+    //             // ListMultiply(&A,&B,&MultiplyTemp, SqList *Multiply_B, SqList *Output);
+    //             PrintList(&MultiplyResult);
+    //             break;
+    //         }
             
-            case 5:
-            break;
-            case 0:
-    printf("********************************         Bye~      ***************************************\n");
-            exit_all(&head_1);
-            getchar();
-            exit(0);  //正常退出
-            break;
-            default:
-    printf("****************************输入错误......请重新输入哦***********************************\n");
-             break;
-        }
-    printf("****************************  是否继续  Y  or   N?   ************************************\n");
-         do
-        {
-           scanf("%c",&yes_no);
-        }
-	while(yes_no!='Y'&&yes_no!='y'&&yes_no!='N'&&yes_no!='n');
-    }
-    while(yes_no=='Y'||yes_no=='y');
+    //         case 5:
+    //         break;
+    //         case 0:
+    // printf("********************************         Bye~      ***************************************\n");
+    //         // exit_all(&head_1);
+    //         getchar();
+    //         exit(0);  //正常退出
+    //         break;
+    //         default:
+    // printf("****************************输入错误......请重新输入哦***********************************\n");
+    //          break;
+    //     }
+    // printf("****************************  是否继续  Y  or   N?   ************************************\n");
+    //      do
+    //     {
+    //        scanf("%c",&yes_no);
+    //     }
+	// while(yes_no!='Y'&&yes_no!='y'&&yes_no!='N'&&yes_no!='n');
+    // }
+    // while(yes_no=='Y'||yes_no=='y');
 }
 
 
