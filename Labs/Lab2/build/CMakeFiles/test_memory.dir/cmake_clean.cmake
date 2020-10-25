5ae8558fc3f2bfc1a1534b4ba0cd6dd17e22466d
BC-Li <251156266@qq.com>
2020-10-25 09:10:02 +0800

    Update Lab2

---
+++ Labs/Lab2/build/CMakeFiles/test_memory.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_memory.dir/test/test_memory.cc.o"
+  "CMakeFiles/test_memory.dir/util/util.cc.o"
+  "CMakeFiles/test_memory.dir/simulation.cc.o"
+  "test_memory.pdb"
+  "test_memory"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_memory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
