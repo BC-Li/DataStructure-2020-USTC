e1a9cbec5993c80c35fa737cadafe1d2049247f7
BC-Li <251156266@qq.com>
2020-12-21 17:19:07 +0800

    Add a test file to read your binary input

--- Labs/Lab3/test.c
+++ Labs/Lab3/test.c
@@ -4,12 +4,10 @@
 #include<stdarg.h>
 #include<assert.h>
 #include<math.h>
-#include<ctype.h>
-#include<omp.h>
+
  char* strlower( char *s)
 {
    char* t;
-
    for(t = s; *s != '\0'; s++)
       *s = (char)tolower(*s);
 
@@ -20,20 +18,7 @@ int main(){
     FILE *fp;
      fp = fopen("USAbin.gr", "rb");
      char in_line[1024];
-    // index_t n = 0;
-    // index_t m = 0;
     int u, v, w;
-
-    // g->n = (int)23947347;
-    // g->m = (int)58333344;
-    // while(fp!=NULL) {
-        // fread(&u, sizeof(int), 1, fp);
-        // // char *line = strlower(in_line);
-        //     fread(&v, sizeof(int), 1, fp);
-        //     fread(&w, sizeof(int), 1, fp);
-        // // sscanf(line, "%d %d %d", &u, &v, &w);
-        // printf("%d %d %d\n",u,v,w);
-    //     break;
     int s[3];
     while(fread(s,sizeof(int),3,fp)){
         u = s[0];
@@ -41,5 +26,4 @@ int main(){
         w = s[2];
         printf("%d %d %d\n",u,v,w);
     }
-    // }
 }
